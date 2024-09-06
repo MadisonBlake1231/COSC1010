be8d93b885240f729a52d7e7301b6bf3da85f286
Madison Blake <madisonblake@westernwyoming.edu>
2024-09-06 13:07:15 -0600

    Sales Tax
    Let me know if I added it in correctly, I think I added it correctly, but I know that the code is running for sure.

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 #
 # Madison Blake
-# 09/01/2024
-# Your First Python Program
-# COSC 1010 
+# 09/06/2024
+# Sales Tax Programming Project
+# COSC 2409 DNT
 #
 
-# This is my first Python program.
-print("Hello World")
+# Variable declarations
+purchase = 0.0
+stateTax = 0.0
+countryTax = 0.0
+totalTax = 0.0
+totalSale = 0.0
+
+# Constants for the state and county tax rates
+STATE_TAX_RATE = 0.05
+COUNTRY_TAX_RATE = 0.025
+
+# Get the amount of the purchase.
+purchase = float(input("Enter the amount of the purchase: "))
+
+# Calculate the state sales tax.
+stateTax = purchase * STATE_TAX_RATE
+
+# Calculate the county sales tax.
+countryTax = purchase * COUNTRY_TAX_RATE
+
+# Calculate the total tax.
+totalTax = stateTax + countryTax
+
+# Calculate the total of the sale.
+totalSale = purchase + totalTax
+
+# Print information about the sale.
+print("Purchase Amount:", format(purchase, '.2f'))
+print("State Tax:", format(stateTax, '.2f'))
+print("Country Tax:", format(countryTax, '.2f'))
+print("Total Tax:", format(totalTax, '.2f'))
+print("Sale Total:", format(totalSale, '.2f'))
