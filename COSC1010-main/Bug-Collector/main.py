3bc48eb40be5bbc03e7f2f6c5cd0312d0dd17c34
Madison Blake <madisonblake@westernwyoming.edu>
2024-09-23 12:17:07 -0600

    Add files via upload

--- COSC1010-main/Bug-Collector/main.py
+++ COSC1010-main/Bug-Collector/main.py
@@ -1,11 +1,18 @@
 #
-# Name
-# Date
+# Madison Blake
+# 09-23-2024
 # Bug Collector Programming Project
 # COSC 2409 DNT
 #
 # Initialize variables for bugs and total number of bugs collected.
+total = 0
+bugs = 0
 
 # Get number of bugs collected each day using a for loop.
+for day in range(1, 8) :
+    print('Enter the bugs collected on day', day)
+    bugs = int(input())
+    total = total + bugs
 
 # Display the total number of bugs collected.
+print ('You collected a total of', total, 'bugs.')
